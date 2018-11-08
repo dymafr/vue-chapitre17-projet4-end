963481dea8f3d6f290622c5d0bc11a3e82bc529c
hades <quentindevial19@gmail.com>
2018-11-08 11:12:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <the-header></the-header>
+    <div class="d-flex flex-column w-100">
+      <component :is="page" ></component>
+    </div>
+  </div>
+</template>
+
+<script>
+import { eventBus } from './main';
+
+import TheHeader from './components/TheHeader'
+import User from './components/features/User/User';
+import Admin from './components/features/Admin/Admin';
+
+export default {
+  name: 'app',
+  components: {
+    TheHeader,
+    User,
+    Admin
+  },
+  data() {
+    return {
+      page: eventBus.page
+    }
+  },
+  created() {
+    eventBus.$on('update:page', (page) => {
+      this.page = page;
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
